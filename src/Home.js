ddf76de147a1f59743ae33deefc286ed81973a59
Padmaja Sharma <97097465+Padmajasharma@users.noreply.github.com>
2024-08-01 11:49:27 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Home.scss';
 import BillingSoftware from './components/BillingSoftware';
 import OverlapContainer from './components/OverlapContainer';
-import ProductExplanation from './cmponents/ProductExplanation';
+import ProductExplanation from './components/ProductExplanation';
 
 
 const Home = () => {
